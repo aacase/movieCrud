ce5e672fc8c9f592e83a8cc53616f8e7c4984272
aacase <aacase@gmail.com>
2016-08-24 21:01:29 -0500

    Rewire database connection
    Was linking to the wrong endpoint. Changed from displaying the movie
    database as one entry to displaying all of the seed data properly.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,9 @@
  * Controller of the inMotionApp
  */
 angular.module('inMotionApp')
-  .controller('MainCtrl', function ($scope, $firebaseObject) {
-    var ref = new Firebase("https://inmotionmovies.firebaseio.com/");
-    $scope.data = $firebaseObject(ref);
-    console.log($scope.data)
+  .controller('MainCtrl', function($scope, $firebaseArray) {
+    var ref = new Firebase("https://inmotionmovies.firebaseio.com/movies");
+    //bind firebase data to app scope so we can use it on the dom.
+    $scope.movies = $firebaseArray(ref);
 
   });
