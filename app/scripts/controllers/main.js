91da7944fb4146ae33ed6c42c7c61a3c647f1b02
aacase <aacase@gmail.com>
2016-08-24 23:14:01 -0500

    Add Delete Options
    Added $firebaseObject module in order to access and delete the movies
    in a modular fashion.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,17 @@
  * Controller of the inMotionApp
  */
 angular.module('inMotionApp')
-  .controller('MainCtrl', function($scope, $firebaseArray) {
+  .controller('MainCtrl', function($scope, $firebaseArray, $firebaseObject) {
     var ref = new Firebase("https://inmotionmovies.firebaseio.com/movies");
     //bind firebase data to app scope so we can use it on the dom.
     $scope.movies = $firebaseArray(ref);
+    // $scope.actors = $scope.movies.actors
 
+  //delete a movie from the collection. Personally I would never do this
+   $scope.removeMovie = function(id) {
+
+    var ref2 = new Firebase("https://inmotionmovies.firebaseio.com/movies/"+id);
+    var movie = $firebaseObject(ref2)
+    movie.$remove();
+   };
   });
