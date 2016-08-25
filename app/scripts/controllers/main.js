9f0afc8e92d09bdc95f368abcfb387f9c38c6803
aacase <aacase@gmail.com>
2016-08-24 20:46:30 -0500

    create main view and inject firebase into main view controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,9 @@
  * Controller of the inMotionApp
  */
 angular.module('inMotionApp')
-  .controller('MainCtrl', function () {
-  
+  .controller('MainCtrl', function ($scope, $firebaseObject) {
+    var ref = new Firebase("https://inmotionmovies.firebaseio.com/");
+    $scope.data = $firebaseObject(ref);
+    console.log($scope.data)
+
   });
